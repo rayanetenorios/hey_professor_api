28167658267235882032edab33dc5809dd93ed74
Rayane Tenório <rayanemtenorio@gmail.com>
2024-09-27 18:43:18 -0300

    response format and status

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -6,15 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Question\StoreRequest;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        Question::create([
+        $question = Question::create([
             'user_id'  => auth()->user()->id,
             'status'   => 'draft',
             'question' => $request->question,
         ]);
+
+        return response([
+            'data' => [
+                'id'         => $question->id,
+                'question'   => $question->question,
+                'status'     => $question->status,
+                'created_at' => $question->created_at->format('Y-m-d'),
+                'updated_at' => $question->updated_at->format('Y-m-d'),
+            ]
+        ], Response::HTTP_CREATED);
     }
 }
--- tests/Feature/Questions/StoreQuestionTest.php
+++ tests/Feature/Questions/StoreQuestionTest.php
@@ -85,3 +85,25 @@ describe('validation rules', function() {
             ]);
     });
 });
+
+test('after creating we should return a status 201 with the created question', function() {
+    $user = User::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    $request = postJson(route('questions.store'), [
+        'question' => 'Lorem ipsun jeremias?',
+    ])->assertCreated();
+
+    $question = Question::latest()->first();
+
+    $request->assertJson([
+        'data' => [
+            'id'         => $question->id,
+            'question'   => $question->question,
+            'status'     => $question->status,
+            'created_at' => $question->created_at->format('Y-m-d'),
+            'updated_at' => $question->updated_at->format('Y-m-d'),
+        ]
+    ]);
+});
