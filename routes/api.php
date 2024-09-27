df6b8f9e4e107413cbe1b443124828ba9df4f750
Rayane Tenório <rayanemtenorio@gmail.com>
2024-09-27 15:49:31 -0300

    ajustes rotas

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Authenticated routes
+Route::middleware('auth:sanctum')->group( function() {
+    
+    // region Questions
+    Route::post('questions', fn() => '')->name('questions.store');
+
+    // end region Questions
+});
