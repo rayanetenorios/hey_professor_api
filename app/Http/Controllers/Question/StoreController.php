694a6b4e0aef278e9209bff63868d17ff7899805
Rayane Tenório <rayanemtenorio@gmail.com>
2024-09-27 19:07:54 -0300

    refactoring to user relationships instead of infere the user_id on the creation

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -5,19 +5,16 @@ namespace App\Http\Controllers\Question;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Question\StoreRequest;
 use App\Http\Resources\QuestionResource;
-use App\Models\Question;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        $question = Question::create([
-            'user_id'  => auth()->user()->id,
-            'status'   => 'draft',
-            'question' => $request->question,
-        ]);
+        $question = user()->questions()
+            ->create([
+                'status'   => 'draft',
+                'question' => $request->question,
+            ]);
 
         return QuestionResource::make($question);
     }
